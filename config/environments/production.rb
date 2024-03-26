804e03617cc10c0815038ffc778c07e4dc7b8c27
John Hayes-reed <john.hayes-reed@digital.cabinet-office.gov.uk>
2024-03-26 18:02:48 +0000

    add preprod host to environment configuration files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   OmniAuth.config.full_host = ENV['EDITOR_FULL_URL_ROOT']
+
+
+  config.hosts << "builder.forms.preprod.grid.civilservice.gov.uk"
 end
